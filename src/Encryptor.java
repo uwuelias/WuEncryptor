1a341af87361a11d3707af4992cfc5f40310bea3
Elias Wu Feng <eliasw10@nycstudents.net>
2023-03-13 19:09:29 -0400

    decryptMessage not done

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -59,37 +59,20 @@ public class Encryptor {
     }
 
      public String encryptMessage(String message) {
-        
+        String encrypt = "";
+        int size = numCols * numRows;
+        while (message.length() > 0) {
+            if (size > message.length()) {
+                size = message.length();
+            }
+            fillBlock(message);
+            encrypt += encryptBlock();
+            message = message.substring(size);
+        }
+        return encrypt;
      }
-/*
-     Decrypts an encrypted message. All filler 'A's that may have been
-     *   added during encryption will be removed, so this assumes that the
-     *   original message (BEFORE it was encrypted) did NOT end in a capital A!
-     *
-     *   NOTE! When you are decrypting an encrypted message,
-     *         be sure that you have initialized your Encryptor object
-     *         with the same row/column used to encrypted the message! (i.e.
-     *         the “encryption key” that is necessary for successful decryption)
-     *         This is outlined in the precondition below.
-     *
-     *   Precondition: the Encryptor object being used for decryption has been
-     *                 initialized with the same number of rows and columns
-     *                 as was used for the Encryptor object used for encryption.
-     *
-     *   @param encryptedMessage  the encrypted message to decrypt
-     *
-     *   @return  the decrypted, original message (which had been encrypted)
-     *
-     *   TIP: You are encouraged to create other helper methods as you see fit
-     *        (e.g. a method to decrypt each section of the decrypted message,
-     *         similar to how encryptBlock was used)
-     */
-    /*
+
     public String decryptMessage(String encryptedMessage) {
 
-
     }
-
-     */
-
 }
--- src/EncryptorTester.java
+++ src/EncryptorTester.java
@@ -174,10 +174,6 @@ public class EncryptorTester {
           System.out.println("  ACTUAL: " + actualEncrypted5);
         }
 
-    /*
-        // -------------------------------------
-        // ---- TEST PART D: decryptMessage ----
-        // -------------------------------------
         System.out.println("\n---- TESTING PART D ----");
         Encryptor encryptorTest2 = new Encryptor(2, 3);
         String actualDecrypted1 = encryptorTest2.decryptMessage("Mte eati dmnitgAhA");
@@ -232,7 +228,6 @@ public class EncryptorTester {
           System.out.println("EXPECTED: " + expectedDecrypted6);
           System.out.println("  ACTUAL: " + actualDecrypted6);
         }
-    */
     }
 
     public static void print2DArray(String[][] arr) {
