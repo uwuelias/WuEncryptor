4864ae84a687c3190758ffe40e0e4c3870ba37cd
unknown <BT_3W2_23@BKTECH.LOCAL>
2023-03-13 12:35:08 -0400

    Initial commit

---
+++ src/Encryptor.java
@@ -0,0 +1,95 @@
+public class Encryptor {
+    /** A two-dimensional array of single-character strings, instantiated in the constructor */
+    private String[][] letterBlock;
+
+    /** The number of rows of letterBlock, set by the constructor */
+    private int numRows;
+
+    /** The number of columns of letterBlock, set by the constructor */
+    private int numCols;
+
+    /** Constructor*/
+    public Encryptor(int r, int c) {
+        letterBlock = new String[r][c];
+        numRows = r;
+        numCols = c;
+    }
+
+    public String[][] getLetterBlock() {
+        return letterBlock;
+    }
+
+    /** Places a string into letterBlock in row-major order.
+     *
+     *   @param str  the string to be processed
+     *
+     *   Postcondition:
+     *     if str.length() < numRows * numCols, "A" in each unfilled cell
+     *     if str.length() > numRows * numCols, trailing characters are ignored
+     */
+    public void fillBlock(String str) {
+        int i = 0;
+        for (int row = 0; row < letterBlock.length; row++) {
+            for (int column = 0; column < letterBlock[0].length; column++) {
+                if (i < str.length()) {
+                    letterBlock[row][column] = str.substring(i, i + 1);
+                }
+                else {
+                    letterBlock[row][column] = "A";
+                }
+                i++;
+            }
+        }
+    }
+
+    /** Extracts encrypted string from letterBlock in column-major order.
+     *
+     *   Precondition: letterBlock has been filled
+     *
+     *   @return the encrypted string from letterBlock
+     */
+    public String encryptBlock() {
+        String str = "";
+        for (int column = 0; column < letterBlock[0].length; column++) {
+            for (int row = 0; row < letterBlock.length; row++) {
+                str += letterBlock[row][column];
+            }
+        }
+        return str;
+    }
+
+     public String encryptMessage(String message) {
+        
+     }
+/*
+     Decrypts an encrypted message. All filler 'A's that may have been
+     *   added during encryption will be removed, so this assumes that the
+     *   original message (BEFORE it was encrypted) did NOT end in a capital A!
+     *
+     *   NOTE! When you are decrypting an encrypted message,
+     *         be sure that you have initialized your Encryptor object
+     *         with the same row/column used to encrypted the message! (i.e.
+     *         the “encryption key” that is necessary for successful decryption)
+     *         This is outlined in the precondition below.
+     *
+     *   Precondition: the Encryptor object being used for decryption has been
+     *                 initialized with the same number of rows and columns
+     *                 as was used for the Encryptor object used for encryption.
+     *
+     *   @param encryptedMessage  the encrypted message to decrypt
+     *
+     *   @return  the decrypted, original message (which had been encrypted)
+     *
+     *   TIP: You are encouraged to create other helper methods as you see fit
+     *        (e.g. a method to decrypt each section of the decrypted message,
+     *         similar to how encryptBlock was used)
+     */
+    /*
+    public String decryptMessage(String encryptedMessage) {
+
+
+    }
+
+     */
+
+}
